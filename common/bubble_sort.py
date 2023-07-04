e8a170098df9972c4d660e1358039a432862d2c0
“cylDemo” <yl18318350969@163.com>
2023-07-04 23:07:12 +0800

    Initial commit

---
+++ common/bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print("排序后的数组为为：")
+    for i in range(len(arr)):
+        print("%d" %arr[i])
+
+arr = [56, 25, 78, 32, 14, 69, 54, 65, 98, 12, 24, 66]
+bubble_sort(arr)
